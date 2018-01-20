db7464b08ff91785a3fdbc816277341bb1303dbc
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2018-01-20 15:09:10 +0100

    Add eslint
    Eslint link fix

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "semi": ["off"],
+        "comma-dangle": ["off"],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "jsx-a11y/anchor-is-valid": ["error", {
+            "components": ["Link"],
+            "specialLink": ["hrefLeft", "hrefRight", "to"],
+            "aspects": ["noHref", "invalidHref", "preferButton"]
+        }]
+    },
+    "env": {
+        "browser": true,
+    },
+};
