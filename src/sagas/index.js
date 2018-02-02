c9650424d2eff732ed96ecaefc88f1e10cad624b
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2018-02-02 13:37:08 +0100

    Connect to Firebase and first step of implemeting redux saga

---
+++ src/sagas/index.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+
+function* getSpeedValue() {
+  // const testDatabase = firebase.database().ref('test')
+
+  // try {
+  //   const speedValue = yield testDatabase.child('speed')
+  // }
+}
