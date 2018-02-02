b0700d0de3770d0f4e28314cd2a54c85defc4095
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2018-02-02 13:37:08 +0100

    Connect to Firebase and first step of implemeting redux saga

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDO3jlPsWPyZLTbHAPcVNaGvuZWQOpjemw',
+  authDomain: 'postapp-87a73.firebaseapp.com',
+  databaseURL: 'https://postapp-87a73.firebaseio.com',
+  projectId: 'postapp-87a73',
+  storageBucket: 'postapp-87a73.appspot.com',
+  messagingSenderId: '1070695528382'
+}
