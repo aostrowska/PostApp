f50cf2fed79b0f6198ba6c1ffa6bddeaf3ffd287
AleOla <aleola@MacBook-Pro-AleOla.local>
2020-04-05 21:17:48 +0200

    Update of CRA

---
+++ eslintrc.js
@@ -0,0 +1,11 @@
+{
+  "extends": "airbnb",
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "func-names": ["error", "never"]
+  },
+  "plugins": [
+    "react-hooks"
+  ]
+}
